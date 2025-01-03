41d15b991590e0f95bf91f28bd052801a22e3f3d
PriyankaN <piyu1818@gmail.com>
2025-01-03 09:25:26 +0530

    JAva Day-6

---
+++ src/com/week1/New2Class.java
@@ -0,0 +1,17 @@
+package com.week1;
+
+import com.week2.AccessModifier;
+
+//import com.week2.AccessModifier;
+public class New2Class {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Different package and different class");
+		AccessModifier a1=new AccessModifier();
+		System.out.println(a1.name);
+		
+
+	}
+
+}
