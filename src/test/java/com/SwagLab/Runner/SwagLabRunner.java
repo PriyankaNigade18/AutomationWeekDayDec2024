d86aac599cce9723cab8f248d0b890a8bb37fa76
PriyankaNigade18 <piyu1818@gmail.com>
2025-05-07 09:41:56 +0530

    Cucumber Framework Day-2

--- src/test/java/com/SwagLab/Runner/SwagLabRunner.java
+++ src/test/java/com/SwagLab/Runner/SwagLabRunner.java
@@ -8,7 +8,10 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"src/test/resources/FeatureFiles/01_Login.feature",
 							"src/test/resources/FeatureFiles/02_Inventory.feature"},
-					glue={"com.SwagLab.Steps","com.SwagLab.Hooks"})
+					glue={"com.SwagLab.Steps","com.SwagLab.Hooks"},
+					//plugin={"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+							plugin = {"com.aventstack.chaintest.plugins.ChainTestCucumberListener:" })
+
 public class SwagLabRunner {
 
 }
